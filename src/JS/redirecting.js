04c0843576bd515dd7a6b9302c2356192468bc94
pervezn <nidapervez2020@u.northwestern.edu>
2020-05-13 17:17:14 -0400

    travel, writing, and basic templates created

---
+++ src/JS/redirecting.js
@@ -0,0 +1,40 @@
+window.onload = function(){ 
+    const home = document.getElementById("Home")
+    const projects = document.getElementById("Projects")
+    const movies = document.getElementById("Movies")
+    const photos = document.getElementById("Photos")
+    const writing = document.getElementById("Writing")
+    const travel = document.getElementById("Travel")
+    // const about = document.getElementById("About")
+    // const resume = document.getElementById("Resume")
+
+    console.log("home is: " + home )
+
+    home.onclick = function () {
+        location.href = "../HTML/home.html"
+    }   
+    projects.onclick = function () {
+        location.href = "../HTML/projects.html"
+    }
+    movies.onclick = function () {
+        location.href = "../HTML/movies.html"
+    }  
+    photos.onclick = function () {
+        location.href = "../HTML/photos.html"
+    }  
+    travel.onclick = function () {
+        location.href = "../HTML/travel.html"
+    }  
+    writing.onclick = function () {
+        location.href = "../HTML/writing.html"
+    }  
+    // about.onclick = function () {
+    //     location.href = "../HTML/about.html"
+    // }  
+    // resume.onclick = function () {
+    //     location.href = "../HTML/resume.html"
+    // }  
+
+
+};
+
