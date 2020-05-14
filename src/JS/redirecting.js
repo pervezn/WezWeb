dad99e9e05bc59b9da9b4e5ef43f987cec2dadd2
pervezn <nidapervez2020@u.northwestern.edu>
2020-05-13 21:31:06 -0400

    tropetracker done

--- src/JS/redirecting.js
+++ src/JS/redirecting.js
@@ -8,28 +8,41 @@ window.onload = function(){
     const about = document.getElementById("About")
     // const resume = document.getElementById("Resume")
 
-    console.log("home is: " + home )
+    console.log("HEREEEEE")
+    
 
     home.onclick = function () {
+        console.log("home clicked" )
         location.href = "../HTML/home.html"
+        
     }   
     projects.onclick = function () {
+        console.log("projects clicked")
         location.href = "../HTML/projects.html"
+        
     }
     movies.onclick = function () {
+        console.log("movies clicked" )
         location.href = "../HTML/movies.html"
+        
     }  
     photos.onclick = function () {
+        console.log("photos clicked" )
         location.href = "../HTML/photos.html"
+        
     }  
     travel.onclick = function () {
+        console.log("travel clicked" )
         location.href = "../HTML/travel.html"
     }  
     writing.onclick = function () {
+        console.log("writing clicked")
         location.href = "../HTML/writing.html"
     }  
     about.onclick = function () {
+        console.log("about clicked" )
         location.href = "../HTML/about.html"
+        
     }  
     // resume.onclick = function () {
     //     location.href = "../HTML/resume.html"
